3f52203316c7f9a29eac61542fc52c7cf0b3c249
Alessandra Lima <alexiakidd.dev@gmail.com>
2021-05-27 12:21:19 -0300

    feat: creating task create component

---
+++ src/cadastrar/cadastrar-tarefa.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+function CadastrarTarefa() {
+	return <h1>Cadastrar Tarefa</h1>
+}
+
+export default CadastrarTarefa
---
+++ src/cadastrar/cadastrar-tarefa.test.js
@@ -0,0 +1,9 @@
+import CadastrarTarefa from './cadastrar-tarefa'
+
+describe('Teste do componente de cadastro de tarefas', () => {
+	it('deve renderizar o componente sem erros', () => {
+		const div = document.createElement('div')
+		ReactDOM.render(<CadastrarTarefa />, div)
+		ReactDOM.unmountComponentAtNode(div)
+	})
+})
