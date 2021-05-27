a02bb2b666ed60fc00c0d16cabb1211d3b5c8b01
Alessandra Lima <alexiakidd.dev@gmail.com>
2021-05-27 11:10:17 -0300

    feat: setting project

---
+++ src/gerenciador-tarefas.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import GerenciadorTarefas from './gerenciador-tarefas'
+
+test('renders learn react link', () => {
+	render(<GerenciadorTarefas />)
+	const linkElement = screen.getByText(/learn react/i)
+	expect(linkElement).toBeInTheDocument()
+})
