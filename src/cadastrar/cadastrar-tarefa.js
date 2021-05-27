96a2fc040e29aea3e2de9d6f0fbb9156c49a6132
Alessandra Lima <alexiakidd.dev@gmail.com>
2021-05-27 20:41:52 -0300

    feat: create register form

--- src/cadastrar/cadastrar-tarefa.js
+++ src/cadastrar/cadastrar-tarefa.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form, Jumbotron, Modal } from 'react-bootstrap'
+import { navigate, A } from 'hookrouter'
 
 // import { Container } from './styles';
 
 function CadastrarTarefa() {
-	return <h1>Cadastrar Tarefa</h1>
+	return (
+		<div>
+			<h3 className="text-center">Cadastrar</h3>
+			<Jumbotron>
+				<Form>
+					<Form.Group>
+						<Form.Label>Tarefa</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Digite a tarefa"
+							minLength="5"
+							maxLength="100"
+							required
+						/>
+					</Form.Group>
+					<Form.Control.Feedback type="invalid">
+						A tarefa deve conter ao menos 5 caracteres
+					</Form.Control.Feedback>
+					<Form.Group className="text-center">
+						<Button variant="success" type="submit">
+							Cadastrar
+						</Button>
+						&nbsp;
+						<A href="/" className="btn btn-light">
+							Voltar
+						</A>
+					</Form.Group>
+				</Form>
+			</Jumbotron>
+		</div>
+	)
 }
 
 export default CadastrarTarefa
--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,9 +1,14 @@
+import { A } from 'hookrouter'
 import React from 'react'
 
 // import { Container } from './styles';
 
 function ListarTarefas() {
-	return <h1>Listagem de Tarefas</h1>
+	return (
+		<A href="/cadastrar">
+			<div className="btn btn-success btn-sm">Nova tarefa</div>
+		</A>
+	)
 }
 
 export default ListarTarefas
